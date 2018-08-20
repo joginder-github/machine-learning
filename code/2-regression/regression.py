7cb14ca6309c59f33937d327a030787c2e93b65b
git_chetan <chetanpandit2011@gmail.com>
2018-08-21 00:41:51 +0530

    Regression Prediction.

--- code/2-regression/regression.py
+++ code/2-regression/regression.py
@@ -12,7 +12,7 @@ y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +21,15 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting simple regression to set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+## upto this point we have created a simple linear regression machine
+
+
+# predicting salary with the created model
+y_pred = regressor.predict(X_test)
